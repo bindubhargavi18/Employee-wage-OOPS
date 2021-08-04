168274e1d72691828b4a4efbe56519e23b0e954c
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-05 00:02:51 +0530

    Added:Part time employee

--- EmployeeDailyWage.java
+++ EmployeeDailyWage.java
@@ -1,20 +1,27 @@
 class EmployeeDailyWage
 {
-	private int fulltime=1,wagehr=20,dayhrs=8,dailywage;
-	double emp=Math.floor(Math.random()*10)%2;
-	public boolean empAttendance()
-	{
-		if(emp==fulltime)
-			return true;
-		else
-			return false;
-	}
-	public int dailyWageCal()
+	private int fulltime=1;
+	private int parttime=2,wagehr=20,dayhrs=8,dailywage,parttimehrs=4;
+	private double emp=Math.floor(Math.random()*10)%3;
+	public void dailyWageCal()
 	{		
-		if (empAttendance())
-			return wagehr*dayhrs;
+		if (emp==fulltime)
+		{
+			System.out.println("Employee is fulltime");
+			dailywage=wagehr*dayhrs;
+			System.out.println("Fulltime employee salary:"+dailywage);
+		}
+		else if(emp==parttime)
+		{
+			System.out.println("Employee is parttime");
+			dailywage=wagehr*parttimehrs;
+			System.out.println("Parttime employee salary:"+dailywage);
+		}
 		else
-			return wagehr*0;
+		{
+			System.out.println("Employee is absent");
+			System.out.println("Salary is 0");
+		}
 	}	
 			
 }
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,7 +3,7 @@ class EmployeeWage
 	public static void main(String[] args)
 	{
 		EmployeeDailyWage emp=new EmployeeDailyWage();
-		System.out.println("Employee daily salary:"+emp.dailyWageCal());	
+		emp.dailyWageCal();	
 	}
 	
 }
