d442b2bf72f5f869b8a99e51c2406c5c8afb6254
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-04 23:27:26 +0530

    Added:attendance of employee using method

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,7 +3,17 @@ class EmployeeWage
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to employee wage computation");
+		EmployeeWage emp=new EmployeeWage();
+		emp.employeeAttendance();
 		
 	}
-	
+	public void employeeAttendance()
+	{
+		int fulltime=1;
+		double emp=Math.floor(Math.random()*10)%2;
+		if (emp==fulltime)
+			System.out.println("employee is present");
+		else
+			System.out.println("employee is absent");
+	}
 }
