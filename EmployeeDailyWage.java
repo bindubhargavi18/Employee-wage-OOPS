cc32622c00d82e8ee7238e2fe740fa541d501a3b
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-05 00:27:05 +0530

    Added:calculated wages for month

--- EmployeeDailyWage.java
+++ EmployeeDailyWage.java
@@ -1,22 +1,28 @@
 class EmployeeDailyWage
 {
 	private int wagehr=20,dayhrs=8,dailywage,parttimehrs=4;
+	private int dayspermonth=20,wagepermonth;
 	private int emp=(int)Math.floor(Math.random()*10)%3;
-	public void dailyWageCal()
+	public int dailyWageCal()
 	{		
 		switch(emp)
 		{
 			case 1:
 			 	dailywage=wagehr*dayhrs;
-				System.out.println("employee daily wage:" +dailywage);
-				break;
+				return dailywage;
 			case 2:
 				dailywage=wagehr*parttimehrs;
-				System.out.println("part time empoyee wage:" +dailywage);
-				break;
+				return dailywage;
 			default:
-				System.out.println("employee is absent and salary will be 0");
+				dailywage=0;
+				return dailywage;
 		}
+	}
+	public void wageCalMonth()
+	{
+		dailyWageCal();
+		wagepermonth=dailywage*dayspermonth;
+		System.out.println("Wages per month:"+wagepermonth);
 	}	
 			
 }
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,7 +3,7 @@ class EmployeeWage
 	public static void main(String[] args)
 	{
 		EmployeeDailyWage emp=new EmployeeDailyWage();
-		emp.dailyWageCal();	
+		emp.wageCalMonth();	
 	}
 	
 }
